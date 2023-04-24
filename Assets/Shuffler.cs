5130f094bc6aa6fed8b5bf0b2f60492f95189c0b
Keijiro Takahashi <keijiro@gmail.com>
2023-04-24 14:05:56 +0900

    basic queue

--- Assets/Shuffler.cs
+++ Assets/Shuffler.cs
@@ -3,41 +3,95 @@ using Klak.TestTools;
 
 public sealed class Shuffler : MonoBehaviour
 {
+    #region Editable attributes
+
     [SerializeField] int _baseFps = 24;
     [SerializeField] ImageSource _source = null;
-    [SerializeField] float _frameInterval = 0.1f;
+    [SerializeField] float _flipTime = 0.15f;
+
+    #endregion
+
+    #region Project asset references
+
+    [SerializeField, HideInInspector] Mesh _quadMesh = null;
+    [SerializeField, HideInInspector] Material _baseMaterial = null;
+
+    #endregion
+
+    #region Private objects
 
     const int Size = 512;
 
-    (RenderTexture, RenderTexture) _frames;
-    MaterialPropertyBlock _block;
+    DummyPipeline _pipeline;
+    (RenderTexture, RenderTexture, RenderTexture) _frames;
+    MaterialPropertyBlock _baseProps;
+    float _progress;
+
+    #endregion
+
+    #region MonoBehaviour implementation
 
     async Awaitable Start()
     {
         Application.targetFrameRate = _baseFps;
 
+        _pipeline = new DummyPipeline(_source);
         _frames.Item1 = new RenderTexture(Size, Size, 0);
         _frames.Item2 = new RenderTexture(Size, Size, 0);
-        _block = new MaterialPropertyBlock();
-
-        var renderer = GetComponent<Renderer>();
+        _frames.Item3 = new RenderTexture(Size, Size, 0);
+        _baseProps = new MaterialPropertyBlock();
 
         while (true)
         {
-            _block.SetTexture("_Texture1", _frames.Item1);
-            _block.SetTexture("_Texture2", _frames.Item2);
+            _progress = 0;
+            _baseProps.SetTexture("_Texture1", _frames.Item1);
+            _baseProps.SetTexture("_Texture2", _frames.Item2);
 
-            for (var t = 0.0f; t < _frameInterval; t += Time.deltaTime)
-            {
-                _block.SetFloat("_Progress", t / _frameInterval);
-                renderer.SetPropertyBlock(_block);
-                await Awaitable.NextFrameAsync();
-            }
+            var task1 = _pipeline.ProcessLight(_frames.Item3);
+            var task2 = Awaitable.WaitForSecondsAsync(0.2f);
+            await task1;
+            await task2;
 
-            if (Random.value < 0.1f) await Awaitable.WaitForSecondsAsync(1);
-
-            _frames = (_frames.Item2, _frames.Item1);
-            Graphics.Blit(_source.Texture, _frames.Item1);
+            _frames = (_frames.Item3, _frames.Item1, _frames.Item2);
         }
+    }
+
+    void OnDestroy()
+    {
+        Destroy(_frames.Item1);
+        Destroy(_frames.Item2);
+        Destroy(_frames.Item3);
+    }
+
+    void Update()
+    {
+        _progress = Mathf.Min(1, _progress + Time.deltaTime / _flipTime);
+        _baseProps.SetFloat("_Progress", _progress);
+
+        Graphics.RenderMesh
+          (new RenderParams(_baseMaterial) { matProps = _baseProps },
+           _quadMesh, 0, Matrix4x4.identity);
+    }
+
+    #endregion
+}
+
+public sealed class DummyPipeline
+{
+    ImageSource _source;
+
+    public DummyPipeline(ImageSource source)
+      => _source = source;
+
+    public async Awaitable ProcessLight(RenderTexture dest)
+    {
+        await Awaitable.WaitForSecondsAsync(0.1f);
+        Graphics.Blit(_source.Texture, dest);
+    }
+
+    public async Awaitable ProcessHeavy(RenderTexture dest)
+    {
+        await Awaitable.WaitForSecondsAsync(1);
+        Graphics.Blit(_source.Texture, dest);
     }
 }
