43d277daf0c5535f791112531ed066f1d9489b0f
Keijiro Takahashi <keijiro@gmail.com>
2023-04-25 15:53:41 +0900

    refactoring

--- Assets/Shuffler.cs
+++ Assets/Shuffler.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
-using Klak.TestTools;
+using CancellationToken = System.Threading.CancellationToken;
+using ImageSource = Klak.TestTools.ImageSource;
+using ComputeUnits = MLStableDiffusion.ComputeUnits;
+using SDPipeline = MLStableDiffusion.Pipeline;
 
 public sealed class Shuffler : MonoBehaviour
 {
@@ -10,41 +13,39 @@ public sealed class Shuffler : MonoBehaviour
     [SerializeField] ImageSource _source = null;
     [SerializeField] string _resourceDir = "StableDiffusion";
     [Space]
-    [SerializeField] float _minorTime = 0.175f;
-    [SerializeField] float _majorTime = 1.2f;
+    [SerializeField] float _flipTime = 0.175f;
+    [SerializeField] float _queueLength = 10;
     [Space]
-    [SerializeField] GeneratorConfig _minorConfig = GeneratorConfig.Default;
-    [SerializeField] GeneratorConfig _majorConfig = GeneratorConfig.Default;
+    [SerializeField] string _prompt = "Surrealistic painting by J. C. Leyendecker";
+    [SerializeField] float _strength = 0.7f;
+    [SerializeField] int _stepCount = 7;
+    [SerializeField] float _guidance = 10;
 
     #endregion
 
     #region Project asset references
 
-    [SerializeField, HideInInspector] Mesh _quadMesh = null;
-    [SerializeField, HideInInspector] Material _minorMaterial = null;
-    [SerializeField, HideInInspector] Material _majorMaterial = null;
-    [SerializeField, HideInInspector] ComputeShader _generatorPreprocess = null;
+    [SerializeField, HideInInspector] Mesh _pageMesh = null;
+    [SerializeField, HideInInspector] Material _pageMaterial = null;
+    [SerializeField, HideInInspector] ComputeShader _sdPreprocess = null;
 
     #endregion
 
     #region Private objects
 
-    const int Size = 512;
-
-    string ResourcePath
-      => Application.streamingAssetsPath + "/" + _resourceDir;
+    string ResourcePath => Application.streamingAssetsPath + "/" + _resourceDir;
 
     // Frame queues
-    (RenderTexture minor, RenderTexture major, RenderTexture flap) _activeFrames;
     Queue<RenderTexture> _freeFrames = new Queue<RenderTexture>();
     Queue<RenderTexture> _stockFrames = new Queue<RenderTexture>();
 
-    // Animation
-    (MaterialPropertyBlock minor, MaterialPropertyBlock major) _props;
-    (float minor, float major) _progress;
+    // Page flipping animation
+    (RenderTexture flip, RenderTexture stay) _pageFrames;
+    MaterialPropertyBlock _pageProps;
+    float _pageProgress, _pageSpeed;
 
-    // Image generator (unmanaged)
-    ImageGenerator _generator;
+    // Stable Diffusion pipeline
+    SDPipeline _sdPipeline;
 
     #endregion
 
@@ -52,35 +53,48 @@ public sealed class Shuffler : MonoBehaviour
 
     async Awaitable InitObjects()
     {
+        var width = MLStableDiffusion.Pipeline.Width;
+
+        // Application frame rate setting
         Application.targetFrameRate = _displayFps;
 
-        // Frame queues
-        _activeFrames.minor = new RenderTexture(Size, Size, 0);
-        _activeFrames.flap = new RenderTexture(Size, Size, 0);
+        // Initial frame queue
+        for (var i = 0; i < _queueLength; i++)
+            _freeFrames.Enqueue(new RenderTexture(width, width, 0));
 
-        for (var i = 0; i < _majorTime / _minorTime + 1; i++)
-            _freeFrames.Enqueue(new RenderTexture(Size, Size, 0));
+        // Page flipping animation
+        _pageFrames.flip = new RenderTexture(width, width, 0);
+        _pageFrames.stay = new RenderTexture(width, width, 0);
+        _pageProps = new MaterialPropertyBlock();
 
-        // Animation
-        _props.minor = new MaterialPropertyBlock();
-        _props.major = new MaterialPropertyBlock();
-
-        // Image generator (unmanaged)
-        _generator = new ImageGenerator(_generatorPreprocess, _source);
-        await _generator.InitializeAsync(ResourcePath);
+        // Stable Diffusion pipeline
+        _sdPipeline = new SDPipeline(_sdPreprocess);
+        Debug.Log("Loading the Stable Diffusion mode...");
+        await _sdPipeline.InitializeAsync(ResourcePath, ComputeUnits.CpuAndGpu);
+        Debug.Log("Done.");
     }
 
     void ReleaseObjects()
     {
-        Destroy(_activeFrames.minor);
-        Destroy(_activeFrames.flap);
-        _activeFrames = (null, null, null);
-
         while (_freeFrames.Count > 0) Destroy(_freeFrames.Dequeue());
         while (_stockFrames.Count > 0) Destroy(_stockFrames.Dequeue());
 
-        _generator.Dispose();
-        _generator = null;
+        Destroy(_pageFrames.flip);
+        Destroy(_pageFrames.stay);
+        _pageFrames = (null, null);
+
+        _sdPipeline?.Dispose();
+        _sdPipeline = null;
+    }
+
+    async Awaitable RunSDPipelineAsync(RenderTexture dest, CancellationToken cancel)
+    {
+        _sdPipeline.Prompt = _prompt;
+        _sdPipeline.Strength = _strength;
+        _sdPipeline.StepCount = _stepCount;
+        _sdPipeline.Seed = Random.Range(1, 2000000000);
+        _sdPipeline.GuidanceScale = _guidance;
+        await _sdPipeline.RunAsync(_source.Texture, dest, cancel);
     }
 
     #endregion
@@ -89,7 +103,7 @@ public sealed class Shuffler : MonoBehaviour
 
     async Awaitable Start()
     {
-        var canceller = destroyCancellationToken;
+        var cancel = destroyCancellationToken;
 
         // Initialization
         await InitObjects();
@@ -98,39 +112,34 @@ public sealed class Shuffler : MonoBehaviour
         {
             // Start major frame generation.
             var majorRT = _freeFrames.Dequeue();
-            var majorTask = _generator.RunAsync(_majorConfig, majorRT, canceller);
+            var majorTask = RunSDPipelineAsync(majorRT, cancel);
 
-            // Flip all the stocked minor pages during the major frame generation.
+            // Flip all the stocked pages during the major frame generation.
             while (_stockFrames.Count > 0)
             {
-                _freeFrames.Enqueue(_activeFrames.minor);
-                _activeFrames.minor = _activeFrames.flap;
-                _activeFrames.flap = _stockFrames.Dequeue();
-                _progress.minor = 0;
-                await Awaitable.WaitForSecondsAsync(_minorTime);
+                _freeFrames.Enqueue(_pageFrames.stay);
+                _pageFrames.stay = _pageFrames.flip;
+                _pageFrames.flip = _stockFrames.Dequeue();
+                (_pageProgress, _pageSpeed) = (0, 1);
+                await Awaitable.WaitForSecondsAsync(_flipTime);
             }
 
             // Complete the major frame generation.
             await majorTask;
 
             // Start major frame animation.
-            _freeFrames.Enqueue(_activeFrames.minor);
-            _activeFrames.minor = _activeFrames.flap;
-            _activeFrames.major = _activeFrames.flap = majorRT;
-            _progress.minor = _progress.major = 0;
+            _freeFrames.Enqueue(_pageFrames.stay);
+            _pageFrames.stay = _pageFrames.flip;
+            _pageFrames.flip = majorRT;
+            (_pageProgress, _pageSpeed) = (0, 0.5f);
 
             // Generate minor frames and fill the queue.
             while (_freeFrames.Count > 1)
             {
-                var minorRT = _freeFrames.Dequeue();
-
-                var task1 = _generator.RunAsync(_minorConfig, minorRT, canceller);
-                var task2 = Awaitable.WaitForSecondsAsync(_minorTime);
-
-                await task1;
-                await task2;
-
-                _stockFrames.Enqueue(minorRT);
+                await Awaitable.WaitForSecondsAsync(_flipTime);
+                var rt = _freeFrames.Dequeue();
+                Graphics.Blit(_source.Texture, rt);
+                _stockFrames.Enqueue(rt);
             }
         }
     }
@@ -139,33 +148,16 @@ public sealed class Shuffler : MonoBehaviour
 
     void Update()
     {
-        var dt = Time.deltaTime;
+        var dt = Time.deltaTime * _pageSpeed;
+        _pageProgress = Mathf.Min(1, _pageProgress + dt / _flipTime);
 
-        _progress.minor = Mathf.Min(1, _progress.minor + dt / _minorTime);
-        _progress.major += dt;
-
-        _props.minor.SetTexture("_Texture1", _activeFrames.flap);
-        _props.minor.SetTexture("_Texture2", _activeFrames.minor);
-        _props.minor.SetFloat("_Progress", _progress.minor);
+        _pageProps.SetTexture("_Texture1", _pageFrames.flip);
+        _pageProps.SetTexture("_Texture2", _pageFrames.stay);
+        _pageProps.SetFloat("_Progress", _pageProgress);
 
         Graphics.RenderMesh
-          (new RenderParams(_minorMaterial) { matProps = _props.minor },
-           _quadMesh, 0, Matrix4x4.identity);
-
-        if (_activeFrames.major == null) return;
-
-        var cutoff = (_progress.major - _minorTime * 5) / _majorTime;
-        var lift = (_progress.major - _minorTime);
-        lift = lift * lift * lift * 0.2f;
-
-        if (lift < 0 || cutoff > 1) return;
-
-        _props.major.SetTexture("_Texture", _activeFrames.major);
-        _props.major.SetFloat("_Cutoff", cutoff);
-
-        Graphics.RenderMesh
-          (new RenderParams(_majorMaterial) { matProps = _props.major },
-           _quadMesh, 0, Matrix4x4.Translate(new Vector3(0, 0, -lift)));
+          (new RenderParams(_pageMaterial){ matProps = _pageProps },
+           _pageMesh, 0, Matrix4x4.identity);
     }
 
     #endregion
